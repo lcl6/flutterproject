5723b96f9d74b1812babff578fe5f54a34b4d831
lcl9985 <liancl@dragoninfo.com.cn>
2021-05-07 09:47:28 +0800

    sdfasfds

--- lib/main.dart
+++ lib/main.dart
@@ -11,24 +11,6 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // switch(routName){
-    //   case 'main':
-    //     return MaterialApp(
-    //       title: 'Flutter Demo',
-    //       theme: ThemeData(
-    //         primarySwatch: Colors.blue,
-    //       ),
-    //       home: MyHomePage(title: 'Flutter Demo Home Page'),
-    //     );
-    //   case 'other':
-    //      return Center(
-    //       child: Text('route: $routName', textDirection: TextDirection.ltr),
-    //     );
-    //   default:
-    //     return Center(
-    //       child: Text('route: $routName', textDirection: TextDirection.ltr),
-    //     );
-    // }
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
@@ -41,15 +23,6 @@ class MyApp extends StatelessWidget {
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
